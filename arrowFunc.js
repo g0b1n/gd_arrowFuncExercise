45e360ff467104b4c266088a5cd42726fb6991e0
Govinda Dahal <gobin@gobinmbp.myfiosgateway.com>
2023-06-30 10:39:33 -0400

    arrowFuncExercise

---
+++ arrowFunc.js
@@ -0,0 +1,17 @@
+//ES5 to ES2015 Arrow func shorthand
+
+const double = arr => arr.map(val => val * 2);
+
+//Refactor the following functions to use arrow functions
+
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+  
+  const squareAndFindEvens = numbers => numbers.map(val => val ** 2).filter(square => square % 2 === 0)
